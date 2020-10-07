18abf7877a3fdba118ea8e6d3e94be1131be9876
Taiga MITSUYUKI <mitsuyuki-taiga-my@ynu.ac.jp>
2020-10-07 11:38:35 +0900

    First commit

---
+++ src/p121.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------- ここから --------------
+x = 10 // 3
+print(x)
+
+y = 10 % 3
+print(y)
+
+z = 10 / 3
+print(z)
---
+++ src/p131.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------- ここから --------------
+x = int(input(u"何か整数を入力してね : "))
+print(x)
